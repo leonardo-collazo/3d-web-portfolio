e7264eb94a9313b702dd8eadf3461d6b4ffa21d1
Leonardo Collazo <leonardo.collazo.klenina@gmail.com>
2024-05-30 19:56:48 -0400

    Updated styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 const styles = {
-  paddingX: "sm:px-16 px-6",
-  paddingY: "sm:py-16 py-6",
+  paddingX: "sm:px-10 px-6",
+  paddingY: "sm:py-10 py-6",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
