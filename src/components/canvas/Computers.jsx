e8a38d0b2caac8b2cf4a4517b32f323850656029
Leonardo Collazo <leonardo.collazo.klenina@gmail.com>
2024-05-31 21:06:06 -0400

    Added the computer (3D model) to the Hero section.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
-export default Computers
+  return (
+    <mesh>
+      <hemisphereLight
+        intensity={1.5}
+        groundColor="black"
+      />
+      <pointLight intensity={2.5} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
