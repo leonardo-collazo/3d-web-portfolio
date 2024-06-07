1ad11b8b01627692dffb7efb03ac7ff4f4120d68
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-06-07 13:16:02 -0400

    Updated Hero design.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,9 +9,9 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-content mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
+        <div className="flex flex-col justify-center items-center mt-4">
           <div className="w-5 h-5 rounded-full bg-hero-header" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-1 sm:h-80 h-48 violet-gradient" />
         </div>
 
         <div>
