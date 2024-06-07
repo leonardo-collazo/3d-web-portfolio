c494d5287d51745177a10b40c1bf737bf8c4b0e7
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-06-07 13:15:22 -0400

    Updated styles.

--- src/styles.js
+++ src/styles.js
@@ -4,9 +4,9 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white lg:text-[70px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+    "font-black text-white lg:text-[60px] md:text-[55px] sm:text-[50px] xs:text-[45px] text-[40px] mt-2 leading-[50px] sm:leading-[60px] md:leading-[70px] lg:leading-[75px]",
   heroSubText:
-    "font-medium text-white lg:text-[28px] sm:text-[24px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+    "font-medium text-white lg:text-[24px] md:text-[22px] sm:text-[20px] xs:text-[18px] text-[16px]",
 
   sectionHeadText: "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText: "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
