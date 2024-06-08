d095fa2dbbf8bbc084ff47488ec3dc6c9e57d932
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-06-08 12:15:01 -0400

    Adjusted Loader position.

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -5,7 +5,7 @@ import "./styles.css";
 const Loader = () => {
   return (
     <Html>
-      <div className="cube-loader top-20 right-1/2 relative w-16 h-16 animate-[animate_4s_linear_infinite]">
+      <div className="cube-loader top-14 md:top-16 lg:top-20 right-1/2 relative w-16 h-16 animate-[animate_4s_linear_infinite]">
         <div className="cube-top absolute w-16 h-16 before:content-[''] before:absolute before:w-16 before:h-16 before:blur-[10px] before:shadow-[0_0_10px_#323232,0_0_20px_hsl(280,42%,41%)_19.6%,0_0_30px_#323232,0_0_40px_hsl(283,42%,41%)_19.6%]"></div>
         <div className="cube-wrapper absolute w-full h-full">
           <span
