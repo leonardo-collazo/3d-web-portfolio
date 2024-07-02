a1a1456ee006546de1de2473dad94c683d29f660
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-02 19:00:42 -0400

    Created a card for each service

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
+
+const ServiceCard = ({ index, title, description }) => {
+  return (
+    <Tilt
+      className="w-full max-w-[330px]"
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+    >
+      <motion.div
+        className="w-full p-[2px] rounded-[20px] green-pink-gradient shadow-card"
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      >
+        <div className="min-h-[300px] flex flex-col justify-start items-center gap-4 px-6 py-8 rounded-[20px] bg-tertiary">
+          <h3 className="text-[20px] font-bold leading-7 text-center text-white">{title}</h3>
+          <p className="text-base font-normal leading-6 text-white">{description}</p>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+export { ServiceCard };
