a51a0cce6f1541dd2cdb78d2fb5f217be50a2dc5
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-03 22:55:15 -0400

    Updated ServiceCard

--- src/components/ServiceCard/index.jsx
+++ src/components/ServiceCard/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-import { fadeIn } from "../utils/motion";
+import { fadeIn } from "../../utils/motion";
 
 const ServiceCard = ({ index, title, description }) => {
   return (
