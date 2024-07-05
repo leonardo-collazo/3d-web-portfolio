f2e3294d90bada8f164b340b37dcb082d4f536e9
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-05 11:32:33 -0400

    - Scrolling not working when clicking on a <Link> element with an anchor path, so I replaced them with an <a> element. - Some changes in the style of the navigation bar elements.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="w-full h-navbar paddingX flex items-center fixed top-0 z-20 bg-primary">
       <div className="w-full flex justify-between items-center max-w-content mx-auto">
-        <Link
-          to="/"
+        <a
+          href="/#"
           className="flex items-center gap-2"
           onClick={() => {
-            setActive("");
             window.scrollTo(0, 0);
           }}
         >
@@ -25,29 +22,26 @@ const Navbar = () => {
             className="w-16 h-16 object-contain"
           />
           <p className="text-white text-[18px] font-bold cursor-pointer">Leonardo Collazo</p>
-        </Link>
+        </a>
 
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map(link => (
-            <li key={link.id}>
-              <Link
-                to={`#${link.id}`}
-                className={`${
-                  active === link.title ? "text-white" : "text-secondary"
-                } hover:text-white text-[18px] font-medium cursor-pointer`}
-                onClick={() => setActive(link.title)}
+            <li key={link.title}>
+              <a
+                href={link.path}
+                className="text-[18px] font-medium text-secondary hover:text-white cursor-pointer"
               >
                 {link.title}
-              </Link>
+              </a>
             </li>
           ))}
         </ul>
 
-        <div className="sm:hidden flex flex-shrink-0 justify-end items-center">
+        <div className="flex flex-shrink-0 justify-end items-center sm:hidden">
           <img
             src={toggle ? close : menu}
-            alt="menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointers"
+            alt="menu icon"
+            className="w-7 h-7 object-contain cursor-pointer"
             onClick={() => {
               setToggle(!toggle);
             }}
@@ -56,23 +50,20 @@ const Navbar = () => {
           <div
             className={`${
               toggle ? "flex" : "hidden"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl z-10`}
+            } absolute top-navbar right-0 w-1/2 min-w-[140px] max-w-[200px] m-2 p-6 rounded-xl black-gradient z-10`}
           >
             <ul className="list-none flex flex-col justify-end items-start gap-4">
               {navLinks.map(link => (
-                <li key={link.id}>
-                  <Link
-                    to={`#${link.id}`}
-                    className={`${
-                      active === link.title ? "text-white" : "text-secondary"
-                    } font-medium cursor-pointer text-[16px]`}
+                <li key={link.title}>
+                  <a
+                    href={link.path}
+                    className="text-[16px] font-medium text-white cursor-pointer"
                     onClick={() => {
                       setToggle(!toggle);
-                      setActive(link.title);
                     }}
                   >
                     {link.title}
-                  </Link>
+                  </a>
                 </li>
               ))}
             </ul>
