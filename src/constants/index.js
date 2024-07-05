36bb6113c373cedfd96b9c61eab041b51c14acfc
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-05 11:32:57 -0400

    Updated constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,16 +23,16 @@ import {
 
 export const navLinks = [
   {
-    id: "about",
     title: "About",
+    path: "#about",
   },
   {
-    id: "projects",
     title: "Projects",
+    path: "#projects",
   },
   {
-    id: "contact",
     title: "Contact",
+    path: "#contact",
   },
 ];
 
