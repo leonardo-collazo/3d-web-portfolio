5eda39c5aac84422b1d63d5f61bc9fcecbeb6846
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-08 10:22:46 -0400

    Unused assets deleted

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,8 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
+import cih from "./company/cih.jpg";
+
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -17,11 +19,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
@@ -31,6 +28,7 @@ export {
   github,
   menu,
   close,
+  cih,
   css,
   docker,
   figma,
@@ -44,10 +42,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
