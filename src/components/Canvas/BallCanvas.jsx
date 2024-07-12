51c669af02f20de0d78c299bd8f15446bca325fd
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-11 21:58:14 -0400

    Created BallCanvas component to render each ball

---
+++ src/components/Canvas/BallCanvas.jsx
@@ -0,0 +1,28 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload } from "@react-three/drei";
+
+import { Ball } from "../Primitives/Ball";
+import { BasicLoader } from "../BasicLoader";
+
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="always"
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<BasicLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
+        />
+        <Ball imgUrl={icon}></Ball>
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export { BallCanvas };
