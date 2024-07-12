85bd1b3bd4419c89e6c0c491b6d297dbf42bbe31
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-11 21:56:27 -0400

    Updated ExperienceCard background color.

--- src/components/ExperienceCard/index.jsx
+++ src/components/ExperienceCard/index.jsx
@@ -3,7 +3,7 @@ import { VerticalTimelineElement } from "react-vertical-timeline-component";
 const ExperienceCard = ({ experience }) => {
   return (
     <VerticalTimelineElement
-      contentStyle={{ background: "#58009F", color: "#fff" }}
+      contentStyle={{ background: "#46007e", color: "#fff" }}
       contentArrowStyle={{ borderRight: "10px solid #fff" }}
       iconStyle={{ background: experience.iconBg }}
       date={experience.date}
