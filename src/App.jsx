e933aa6adf271a6233b60b4c874f6bb3f6c167c1
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-11 21:52:02 -0400

    Removed the unnecessary <BrowserRouter> component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,25 @@
-import { BrowserRouter } from "react-router-dom";
-
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components/";
+import { Stars } from "./components/Canvas/Stars";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components/";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
       </div>
-    </BrowserRouter>
+
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+
+      <div className="relative z-0">
+        <Contact />
+        <Stars />
+      </div>
+    </div>
   );
 };
 
