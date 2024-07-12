e931b188592750c177b09b047cbc92123d870c90
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-11 21:50:17 -0400

    Updated the assets.

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,16 +5,12 @@ import close from "./close.svg";
 
 import cih from "./company/cih.jpg";
 
+import angular from "./tech/angular.svg";
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
@@ -29,16 +25,12 @@ export {
   menu,
   close,
   cih,
+  angular,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
-  mongodb,
-  nodejs,
   reactjs,
-  redux,
   tailwind,
   typescript,
   threejs,
