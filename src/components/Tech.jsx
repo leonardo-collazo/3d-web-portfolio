73ed37dff05b841203e4c6608587e572aa794f89
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-11 21:56:57 -0400

    Tech section completed.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { BallCanvas } from "./Canvas/BallCanvas";
+import { SectionWrapper } from "./SectionWrapper";
+
+import { technologies } from "../constants/";
+import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <>
+      <motion.h2
+        className="sectionHeadText"
+        variants={textVariant()}
+      >
+        Tech Skills
+      </motion.h2>
 
-export default Tech
+      <div className="flex flex-row flex-wrap justify-center gap-12 mt-16">
+        {technologies.map(technology => (
+          <div
+            key={technology.name}
+            className="w-28 h-28 "
+          >
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Tech, "tech");
