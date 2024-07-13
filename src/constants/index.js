b33ea9f79d269b4127bc050c1eb813665b09d855
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-13 17:58:45 -0400

    Added id to projects in constants.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -199,6 +199,7 @@ const testimonials = [
 
 const projects = [
   {
+    id: 3,
     name: "Car Rent",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
@@ -220,6 +221,7 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
+    id: 2,
     name: "Job IT",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
@@ -241,6 +243,7 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
+    id: 1,
     name: "Trip Guide",
     description:
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
