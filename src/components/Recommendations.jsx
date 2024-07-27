ae6d0d15c8903a6942795522b9fd0d7890b2ae17
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-27 18:28:08 -0400

    Updated some styles of the Recommendations component.

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -7,8 +7,8 @@ import { recommendations } from "../constants";
 
 const Recommendations = () => {
   return (
-    <div className="mt-12 rounded-[20px] bg-black-100">
-      <div className="min-h-[300px] padding rounded-2xl bg-tertiary">
+    <div className="mt-2 sm:mt-8 rounded-[20px] bg-black-100">
+      <div className="min-h-[300px] sm:px-16 px-6 sm:py-10 py-8 rounded-2xl bg-tertiary">
         <motion.h2
           className="sectionHeadText"
           variants={textVariant()}
@@ -17,10 +17,10 @@ const Recommendations = () => {
         </motion.h2>
       </div>
 
-      <div className="flex flex-wrap gap-7 -mt-20 pb-14 paddingX">
+      <div className="flex flex-col gap-7 -mt-24 xs:-mt-20 sm:-mt-16 lg:-mt-24 pb-14 paddingX">
         {recommendations.map((recommendation, index) => (
           <RecommendationCard
-            key={recommendation.name}
+            key={recommendation.id}
             index={index}
             {...recommendation}
           />
