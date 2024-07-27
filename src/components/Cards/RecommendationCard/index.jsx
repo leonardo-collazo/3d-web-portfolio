442d4d32428937f263eff1aa591a1cc39dea9cf7
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-27 18:28:30 -0400

    Completed RecommendationCard component.

--- src/components/Cards/RecommendationCard/index.jsx
+++ src/components/Cards/RecommendationCard/index.jsx
@@ -1,24 +1,42 @@
 import { motion } from "framer-motion";
 
-import { fadeIn } from "../../../utils/motion";
+import {
+  toRight,
+  springAnimation,
+  animationDelay,
+  animationDuration,
+  fadeIn,
+} from "../../../utils/motion";
 
-const RecommendationCard = ({ index, recommendation, name, designation, company, image }) => {
+const RecommendationCard = ({ index, recommendation, name, image, linkedInProfile }) => {
   return (
     <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className="w-full xs:w-[320px] p-10 rounded-3xl bg-black"
+      variants={fadeIn(toRight, springAnimation, index * animationDelay, animationDuration)}
+      className="w-full p-10 rounded-3xl bg-black"
     >
       <p className="text-5xl font-black text-white">"</p>
+      <p className="text-lg tracking-wider text-white whitespace-pre-line">{recommendation}</p>
 
-      <div className="mt-1">
-        <p>{recommendation}</p>
+      <div className="flex justify-start items-center gap-10 mt-7">
+        <a
+          href={linkedInProfile}
+          target="_blank"
+          className="text-base font-medium text-white"
+        >
+          <span className="blue-text-gradient">@ </span>
+          <span className="hover:blue-text-gradient">{name}</span>
+        </a>
 
-        <div className="flex justify-between items-center gap-1 mt-7">
-          <div className="flex flex-1 flex-col">
-            <span>@</span> {name}
-            <p></p>
-          </div>
-        </div>
+        <a
+          href={linkedInProfile}
+          target="_blank"
+        >
+          <img
+            src={image}
+            alt={name}
+            className="w-12 h-12 object-cover rounded-full"
+          />
+        </a>
       </div>
     </motion.div>
   );
