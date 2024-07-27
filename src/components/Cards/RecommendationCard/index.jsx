198765620090faeeb0d184a2f28ac249c53743c3
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-27 00:14:40 -0400

    Progress in the development of the RecommendationCard component.

--- src/components/Cards/RecommendationCard/index.jsx
+++ src/components/Cards/RecommendationCard/index.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { motion } from "framer-motion";
 
-const RecommendationCard = () => {
-  return <div>RecommendationCard</div>;
+import { fadeIn } from "../../../utils/motion";
+
+const RecommendationCard = ({ index, recommendation, name, designation, company, image }) => {
+  return (
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full xs:w-[320px] p-10 rounded-3xl bg-black"
+    >
+      <p className="text-5xl font-black text-white">"</p>
+
+      <div className="mt-1">
+        <p>{recommendation}</p>
+
+        <div className="flex justify-between items-center gap-1 mt-7">
+          <div className="flex flex-1 flex-col">
+            <span>@</span> {name}
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export { RecommendationCard };
