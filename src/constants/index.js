8c97c23a64ea7fc9a2eafdf7ecaed80471273884
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-07-26 22:41:17 -0400

    Updated constants.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -249,7 +249,7 @@ const projects = [
 
 const recommendations = [
   {
-    testimonial:
+    recommendation:
       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
     name: "Sara Lee",
     designation: "CFO",
@@ -257,7 +257,7 @@ const recommendations = [
     image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
-    testimonial:
+    recommendation:
       "I've never met a web developer who truly cares about their clients' success like Rick does.",
     name: "Chris Brown",
     designation: "COO",
@@ -265,7 +265,7 @@ const recommendations = [
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
-    testimonial:
+    recommendation:
       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
     name: "Lisa Wang",
     designation: "CTO",
