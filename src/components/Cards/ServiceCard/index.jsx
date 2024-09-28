d428fd2c5c74ecf2ffe1d8ad4015fd368b84b346
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-09-28 09:03:29 -0400

    Less x-padding in ServiceCard.

--- src/components/Cards/ServiceCard/index.jsx
+++ src/components/Cards/ServiceCard/index.jsx
@@ -28,7 +28,7 @@ const ServiceCard = ({ index, title, description }) => {
           cardAnimationDuration
         )}
       >
-        <div className="min-h-[400px] flex flex-col justify-start items-center gap-4 px-6 py-8 rounded-[20px] bg-tertiary">
+        <div className="min-h-[400px] flex flex-col justify-start items-center gap-4 px-4 py-8 rounded-[20px] bg-tertiary">
           <h3 className="text-[20px] font-bold leading-7 text-center text-white">{title}</h3>
           <p className="text-base font-normal leading-6 text-white">{description}</p>
         </div>
